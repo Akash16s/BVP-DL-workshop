1cfd29a0c2488bbabdf074281c74575f009dfcdf
Vasudev Singh <vasu.singh002@gmail.com>
2019-01-29 10:04:39 +0530

    read_glove_func

---
+++ glove_vec.py
@@ -0,0 +1,12 @@
+def read_glove_vectors(glove_pretrained_file):
+    word_embeddings_matrix = {}
+
+    f = open(glove_pretrained_file)
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        word_embeddings_matrix[word] = coefs
+    f.close()
+
+    return word_embeddings_matrix
